eefb57f23ddde11dc6390f9d86ea2b0a50f96fa7
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-01 17:39:48 -0300

    Add files via upload

--- Models/Sce.cs
+++ Models/Sce.cs
@@ -11,18 +11,19 @@ namespace Workers.Models
         {
             string sql = "select EXTERNORDERKEY,WHSEID,STATUS FROM [LPNFD].[" + WpAlmacen + "].[ORDERS] WHERE EXTERNORDERKEY = '" + WpOrdenExterna1 + "'";
             int count = 0;
-            using (SqlConnection connection = new SqlConnection(@"Data Source=DBSCEECOMPROD;Initial catalog=LPNFD;Integrated Security=true"))
+            //using (SqlConnection connection = new SqlConnection(@"Data Source=SQLSCECYPESRV;Initial catalog=LPNFD;Integrated Security=true"))
+            using (SqlConnection connection = new SqlConnection(@"Data Source=DBSCEFARMATEST;Initial catalog=LPNFD;Integrated Security=true"))
             {
                 connection.Open();
                
                 SqlCommand cmd = new SqlCommand(sql, connection);
                 SqlDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("---------------SCE--------------------");
                 try
                 {
                     while (reader.Read())
                     {
-                        count++;
-                        Console.WriteLine("---------------SCE--------------------");
+                        count++;                      
                         Console.WriteLine(String.Format("EXTERNORDERKEY: {0},WHSEID: {1},STATUS: {2}",
                         reader["EXTERNORDERKEY"], reader["WHSEID"], reader["STATUS"]));// etc
                     }
