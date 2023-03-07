9e0359588f3a9eaef3bfd53d99a50e9fde4d1fd5
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-07 18:04:25 -0300

    Add files via upload

--- Models/CollectionMongo.cs
+++ Models/CollectionMongo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +9,190 @@ namespace Workers.Models
 {
     public class CollectionMongo
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string? _id { get; set; }   // this maps to `_id` in the document       
         public int idtransaccion { get; set; }
         public string estado { get; set; }
+        public string propietario { get; set; }
+        public string Razon { get; set; }
+        public string remito { get; set; }
+        //public DateTime fechacreacion { get; set; }
+        //[BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        //public string Id { get; set; }
+        //[BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        //public DateTime CreatedOn { get; set; }
+        //public string Description { get; set; }
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        [BsonDateTimeOptions]
+        public DateTime fechacreacion { get; set; }
+        public object request { get; set; }
+        public object response { get; set; }
+        public string razon { get; set; }
+        public DateTime fechamodificacion { get; set; }
+        //public object[] request { get; set; }
+        //public object[] response { get; set; }
+   
+
+        public class Articulo
+                
+        {
+            public string codigo { get; set; }
+            public string propietario { get; set; }
+            public Lote lote { get; set; }
+        }
+
+        public class Destinatario
+        {
+            public string numerodedocumento { get; set; }
+            public string iddestinatario { get; set; }
+            public string idinternocliente { get; set; }
+            public string nombrecompleto { get; set; }
+            public string email { get; set; }
+            public List<Telefono> telefonos { get; set; }
+            public string tipodedocumento { get; set; }
+            public string contacto { get; set; }
+        }
+
+        public class Detalle
+        {
+            public Articulo articulo { get; set; }
+            public string numeropedido { get; set; }
+            public string unidadmedida { get; set; }
+            public string lineaexterna { get; set; }
+            public int unidades { get; set; }
+            public object datosadicionales { get; set; }
+        }
+
+        public class Detalleordendecompra
+        {
+            public string gs1numerodelineadecliente { get; set; }
+            public string gs1ordendecompra { get; set; }
+            public string gs1fechaordendecompra { get; set; }
+            public string gs1numerolinea { get; set; }
+            public string gs1tipomaterial { get; set; }
+            public string gs1cantidadpedida { get; set; }
+        }
+
+        public class Direccion
+        {
+            public string calle { get; set; }
+            public string numero { get; set; }
+            public string codigopostal { get; set; }
+            public string localidad { get; set; }
+            public string provincia { get; set; }
+            public string pais { get; set; }
+            public string piso { get; set; }
+            public string departamento { get; set; }
+            public string referenciadedomicilio { get; set; }
+            public object componentesdedireccion { get; set; }
+        }
+
+        public class Fechacreacion
+        {      
+            public DateTime date { get; set; }
+        }
+
+        public class Fechamodificacion
+        {
+            [JsonProperty("$date")]
+            public DateTime date { get; set; }
+        }
+
+        public class Id
+        {
+            [JsonProperty("$oid")]
+            public string oid { get; set; }
+        }
+
+        public class Idtransaccion
+        {
+            [JsonProperty("$numberLong")]
+            public string numberLong { get; set; }
+        }
+
+        public class Lote
+        {
+            public string codigo { get; set; }
+            public string lotedefabricante { get; set; }
+            public string fechadevencimiento { get; set; }
+            public Otrosdatos otrosdatos { get; set; }
+        }
+
+        public class Otrosdatos
+        {
+            public object metadatos { get; set; }
+        }
+
+        public class Pedido
+        {
+            public string propietario { get; set; }
+            public string clientepadre { get; set; }
+            public string idpedido { get; set; }
+            public string numero { get; set; }
+            public string tipo { get; set; }
+            public string prioridaddepreparacion { get; set; }
+            public string fechapedido { get; set; }
+            public string fechaentrega { get; set; }
+            public string remito { get; set; }
+            public string idcliente { get; set; }
+            public string referenciacliente { get; set; }
+            public string codigotransportista { get; set; }
+            public string acondicionamientosecundario { get; set; }
+            public string motivo { get; set; }
+            public string ramodeldestinatario { get; set; }
+            public string idejecucion { get; set; }
+            public string embalaje { get; set; }
+            public object notas { get; set; }
+            public string admitecambiolotedirigido { get; set; }
+            public string admitepickingparcial { get; set; }
+            public bool imprimedocumentacion { get; set; }
+            public string nrovale { get; set; }
+            public string valordeclarado { get; set; }
+            public object camposlibres { get; set; }
+            public string sociocomercial { get; set; }
+            public string mododetransporte { get; set; }
+            public Destinatario destinatario { get; set; }
+            public Direccion direccion { get; set; }
+            public List<Detalle> detalles { get; set; }
+            public object datosadicionales { get; set; }
+            public Detalleordendecompra detalleordendecompra { get; set; }
+        }
+
+        public class Request
+        {
+            public string planta { get; set; }
+            public string almacen { get; set; }
+            public string almacensap { get; set; }
+            public string contrato { get; set; }
+            public Pedido pedido { get; set; }
+        }
+
+        public class Response
+        {
+            public Idtransaccion idtransaccion { get; set; }
+        }
+
+        public class Root
+        {
+            public Id _id { get; set; }
+            public Request request { get; set; }
+            public Response response { get; set; }
+            public string estado { get; set; }
+            public string razon { get; set; }
+            public DateTime fechacreacion { get; set; }
+            public Fechamodificacion fechamodificacion { get; set; }
+        }
+
+        public class Telefono
+        {
+            public int tipo { get; set; }
+            public string numero { get; set; }
+        }
+
+
+
     }
 }
--- Models/Mongo.cs
+++ Models/Mongo.cs
@@ -5,42 +5,146 @@ using System.Linq;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Threading.Tasks;
+using MongoDB.Driver.Linq;
+using static Workers.Models.CollectionMongo;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Workers.Models
 {
     public class Mongo
     {     
 
-        internal static List<CollectionMongo> GetMongoCollection()
+        internal static async Task<List<CollectionMongo>> GetMongoCollectionAsync()
         {         
             List<CollectionMongo> listRange = new List<CollectionMongo>();
             try
             {
-                Console.WriteLine("entra a mongo 19");
-                var client = new MongoClient("mongodb://10.20.7.44:27017");
-                //var client = new MongoClient("mongodb://10.20.2.46:27017"); proddddddddd
-                Console.WriteLine("conecto a mongo 21");
-                Console.WriteLine("entra a mongo 22");
+                //var client = new MongoClient("mongodb://10.20.7.44:27017");
+                var client = new MongoClient("mongodb://10.20.2.46:27017"); //proddddddddd
                 List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();
-                Console.WriteLine("entra a mongo 23");
                 var database = client.GetDatabase("APIAlmacenes");
-                Console.WriteLine("entra a mongo 24");
 
-                var collection = database.GetCollection<BsonDocument>("TransaccionesPedidos");
+                var collection = database.GetCollection<CollectionMongo>("TransaccionesPedidos");
 
-                var list = collection.Find(new BsonDocument())
-                     .Limit(2) //retrive only two documents
-                    .ToList();
-        
-                foreach (var docs in list)
+
+
+                //foreach (var item in resultssss)
+                //{
+
+                //}
+                var Request = database.GetCollection<CollectionMongo.Request>("TransaccionesPedidos");
+                var respone = database.GetCollection<CollectionMongo.Response>("Id.Pedido.propietario");
+                var root = database.GetCollection<CollectionMongo.Root>("TransaccionesPedidos");
+                var resultsss = Request.Find(x => x.pedido.remito == "").FirstOrDefault();
+
+
+                //    CollectionMongo results =
+                //from movie in Request.AsQueryable()
+                //where movie.almacen.Contains("Ryan Reynolds")
+                //from cast in Pedido.AsQueryable()
+                //where cast.propietario == "NARANJA"
+                //group movie by cast into g
+                //select new { Cast = g.Key, Sum = g.Sum(x => x.pedido) };
+
+
+
+                //var q = from channel in Request.AsQueryable()
+                //        from episode in channel.
+                //        .Where(x => x.propietario == "NARANJA")
+                //        select new requesta()
+                //        {
+                //            propietario = episode.propietario
+                //        };
+
+                //var min = new DateTime(2023, 01, 01, 22, 0, 0);
+                //var max = (new DateTime(2023, 01, 20, 23, 0, 0));
+
+                //[BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+                string dateTime = "2023-03-06T18:04:56.596+00:00";
+                DateTime dt = Convert.ToDateTime(dateTime);
+                //string dateTimes = "2023-02-01";
+                //DateTime dts = Convert.ToDateTime(dateTimes);
+                string nuevo = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt");
+                DateTime s = Convert.ToDateTime(nuevo);
+
+                //var t= root.AsQueryable().Where(x => x.fechacreacion > dt && x.estado != "PENDIENTE")
+                //    .GroupBy(a => a.request.pedido.remito).Select(a => a.First());
+
+
+                //.Select(x => x.request.pedido.remito).Distinct().Select(x => new SelectListItem { Text = x });
+
+
+
+                var tweetsFiltrados = (from c in root.AsQueryable()
+                                       where c.fechacreacion > dt && c.estado != "PENDIENTE"
+                                       select new
+                                       {
+                                           remito = c.request.pedido.remito,
+                                           estado = c.estado,
+                                           propietario = c.request.pedido.propietario,
+                                           razon = c.razon,
+                                            fecha = c.fechacreacion
+
+                                       })
+                                        .Distinct()
+        .ToList();
+                //int a = tweetsFiltrados.Count();
+                foreach (var item in tweetsFiltrados)
                 {
-                    //_logger.LogInformation(docs.ToString());
-                    //Console.WriteLine("Idtransaccion: " + docs["response"]["idtransaccion"] + "  Estado: " + docs["estado"]);
-
-                    listRange.Add(new CollectionMongo() { idtransaccion = (int)Convert.ToInt64(docs["response"]["idtransaccion"]), estado = (string)docs["estado"] });
-                    //_logger.LogInformation(docs.ToString());
-                    //Console.WriteLine(docs);
+                    //Console.WriteLine("{0},{1},{2},{3},{4}", item.remito, item.propietario, item.estado, item.fecha, item.razon);
+                    listRange.Add(new CollectionMongo() { remito = Convert.ToString(item.remito), propietario = Convert.ToString(item.propietario), estado = Convert.ToString(item.estado), razon = Convert.ToString(item.razon), fechacreacion = Convert.ToDateTime(item.fecha) });
                 }
+
+                //var builder = Builders<CollectionMongo>.Filter;
+             
+                //var pro = IMongoCollection<Request>(collection);
+                //where pro.fechacreacion > dt && pro.fechacreacion < dts
+                //var filter = builder.Eq("request.pedido.propietario", "NARANJA");
+                //filter &= builder.Gt("fechacreacion", "2023-01-01T00:00:00.000+00:00");
+                //var filter = builder.Gt("fechacreacion", "2023-03-06T00:00:00.000+00:00");
+                //filter &=builder.Eq("request.pedido.propietario", "NARANJA"); 
+           
+
+
+                //find the ID of the Godfather movie...
+
+               // dynamic theGodfather = await collection.FindAsync(filter);
+
+               // var options = new FindOptions<BsonDocument>
+               // {
+               //     // Get 100 docs at a time
+               //     BatchSize = 200
+               // };
+               // int count = 0;
+               // using (var cursor = await collection.FindAsync<BsonDocument>(filter))
+
+               // {
+
+               //     while (await cursor.MoveNextAsync())
+
+               //     {
+
+               //         var batch = cursor.Current;
+                    
+               //         foreach (var document in batch.AsEnumerable())
+
+               //         {
+               //             var idtransaccion = (document[2][0]);
+               //             var propietario = (document[1][4][0]);
+               //             var estado = (document[3]);
+               //             var razon = (document[4]);
+               //             var fecha = (document[5]);
+               //             listRange.Add(new CollectionMongo() { idtransaccion =Convert.ToInt32(idtransaccion), propietario = Convert.ToString(propietario), estado =Convert.ToString(estado), razon = Convert.ToString(razon), fechacreacion = Convert.ToDateTime(fecha) });
+               //             Console.WriteLine("{0},{1},{2},{3},{4}", idtransaccion,propietario, estado,fecha,razon);
+
+               //             count++;
+
+               //         }
+
+               //     }
+
+               // }
+          
             }
             catch (Exception ex)
             {
@@ -50,6 +154,11 @@ namespace Workers.Models
             }            
 
             return listRange;
+        }
+
+        private static object IMongoCollection<T>(IMongoCollection<CollectionMongo> collection)
+        {
+            throw new NotImplementedException();
         }
 
         //internal static List<string> GetMongoCollection()
